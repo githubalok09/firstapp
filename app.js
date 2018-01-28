37a372eed61c54ecd2d40d08474ac4f5adbbd70d
Alok <tiwari.kumaralok@gmail.com>
2018-01-28 13:36:10 +0400

    first demo

--- app.js
+++ app.js
@@ -4,34 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+
 var index = require('./routes/index');
-var testroutes = require('./routes/testroutes');
 var users = require('./routes/users');
 
 var app = express();
-var db = null;
-//DB Connectivity
-/*mongoose.connect('mongodb://localhost:27017/test');
-var db = mongoose.connection;*/
-/*db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected');
-});*/
-
-/*var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://localhost:27017/alok', function(err,database) {
-    if(err) { console.error(err) }
-    console.log('connected');
-    db = database // once connected, assign the connection to the global variable
-})*/
 
 // view engine setup
-var exphbs = require('express-handlebars');
-app.engine('hbs', exphbs({defaultLayout: 'default',extname: '.hbs'})); 
-app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -42,9 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/testroutes',testroutes);
 app.use('/users', users);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
